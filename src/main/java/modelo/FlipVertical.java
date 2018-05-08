c8a95905c610606176cdb6a65fa3d216bb80f76e
nicolasalexander <nicolasalexander@niseteocurra>
2018-05-08 18:38:47 -0300

    flip vertical

--- src/main/java/modelo/FlipVertical.java
+++ src/main/java/modelo/FlipVertical.java
@@ -1,9 +1,22 @@
 package modelo;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
 public class FlipVertical implements Filtro {
 
     @Override
     public Imagen aplicarFiltro(Imagen imagen) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Color[][] c=imagen.getBitMap();    
+        int rows = c.length;
+        int cols = c[0].length;
+        Color[][] c1= new Color[rows][cols];
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 0; j--) {
+                c1[rows - 1 - i][cols - 1 - j] = c[i][j];
+            }
+        }
+        imagen.setBitMap(c1);
+        return imagen;
     }
 }

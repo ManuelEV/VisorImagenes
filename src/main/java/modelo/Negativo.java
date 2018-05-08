b0beb66ed159c333535965fbb33fc16e6b13582d
Felipe <Felipe@FELIPE>
2018-05-08 18:14:00 -0300

    se agrego el filtro negativo

--- src/main/java/modelo/Negativo.java
+++ src/main/java/modelo/Negativo.java
@@ -1,9 +1,30 @@
 package modelo;
 
-public class Negativo implements Filtro {
+import java.awt.Color;
 
+public class Negativo implements Filtro {
+    
     @Override
     public Imagen aplicarFiltro(Imagen imagen) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Color[][]img = imagen.getBitMap();
+        
+        for(int i=0;i<imagen.getAncho();i++){
+            
+          for(int j=0;j<imagen.getAlto();j++){
+
+                Color colorAntiguo = img[i][j];
+
+                int r = Math.abs(colorAntiguo.getRed() - 255);
+                int g = Math.abs(colorAntiguo.getGreen() - 255);
+                int b = Math.abs( colorAntiguo.getBlue() - 255);
+                
+                img[i][j]= new Color(r,g,b);              
+                imagen.setBitMap(img);
+                
+            }
+        }      
+        
+        return imagen;
     }
 }

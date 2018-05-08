d2e4b024f14a6a0a0c7b6f4fe80f673c4d6fad67
GabrielGuzman05 <g.guzman05@ufromail.cl>
2018-05-08 18:11:49 -0300

    Generar primera versiòn la UI Ver Imagen

--- src/main/java/vista/PanelImagen.java
+++ src/main/java/vista/PanelImagen.java
@@ -1,20 +1,23 @@
 package vista;
 
-import java.awt.Color;
-import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import modelo.Imagen;
 //dev
 public class PanelImagen extends JPanel {
 
     private Imagen img;
+    private ImageIcon imgIcon;
     
-    public PanelImagen(Imagen im) {
-        this.img = im;
+    public PanelImagen() {
+        this.img = new Imagen("data/imagen.bmp");
         this.inicializarComponentes();
     }
     
     private void inicializarComponentes(){
-        this.setBackground(Color.BLUE);
+        imgIcon = new ImageIcon(img.obtenerImagen());
+        JLabel etiqueta = new JLabel(imgIcon);
+        this.add(etiqueta);
     }
 }

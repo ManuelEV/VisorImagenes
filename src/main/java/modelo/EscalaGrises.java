c43710cb237745213fd4670f3e49317a092f64fe
catalinaaguayo <catalinaaguayo@macbook-pro-de-catalina.local>
2018-05-08 17:32:35 -0300

    Primer avance escala de grises

--- src/main/java/modelo/EscalaGrises.java
+++ src/main/java/modelo/EscalaGrises.java
@@ -1,9 +1,26 @@
 package modelo;
 
+import java.awt.Color;
+
 public class EscalaGrises implements Filtro {
 
     @Override
     public Imagen aplicarFiltro(Imagen imagen) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        imagen.getBitMap();
+        for (int i = 0; i < imagen.getBitMap().length ; i++) {
+            for (int j = 0; j < imagen.getBitMap()[i].length; j++) {
+                Color pixelColor = new Color(imagen.getBitMap()[i][j].getRGB());
+                
+                int red = pixelColor.getRed();
+                int blue = pixelColor.getBlue();
+                int green= pixelColor.getGreen();
+                
+                int mediaRGB = (red + blue + green)/3;
+                
+                int mediaRGBInteger = (mediaRGB << 16) | (mediaRGB << 8) | mediaRGB;
+                
+                //imagen.getBitMap()[i][j].setBitMap()[]
+            }
+        }
     }
 }
